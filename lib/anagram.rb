95e5bcdb63a33f3b03a93b97c7e8cb8ca77e0d1c
Nikolai Pawlowski <nikolaip@gmail.com>
2017-11-07 21:59:44 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,13 @@
 # Your code goes here!
 class Anagram
 
-  attr_accessor :word
-
-  def initialize(word)
-    @word = word
-  end
-
-  def match(words)
-    word_to_check = @word.split("").sort
-    words.select { |w| w.split("").sort == word_to_check }
-  end
-
+  # attr_accessor :word
+  # 
+  # def initialize(word)
+  #   @word = word
+  # end
+  # 
+  # def match(words)
+  #   words.select { |word| word.split("").sort == self.word.split("").sort }
+  # end
 end
