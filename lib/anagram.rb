7ad0974275e133e84612aea073865cf9c611c295
Nikolai Pawlowski <nikolaip@gmail.com>
2017-11-07 21:59:49 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,11 +2,11 @@
 class Anagram
 
   # attr_accessor :word
-  # 
+  #
   # def initialize(word)
   #   @word = word
   # end
-  # 
+  #
   # def match(words)
   #   words.select { |word| word.split("").sort == self.word.split("").sort }
   # end
